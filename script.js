35c7bd215456d8dfd80f4734597bcae9658da2a8
ChriGalipo <161745461+ChriGalipo@users.noreply.github.com>
2024-12-02 14:22:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -42,12 +42,29 @@ document.addEventListener("DOMContentLoaded", () => {
         noButton.style.height = "60px";
     });
 
+    noButton.addEventListener("click", () => {
+        const form = noButton.closest(".form-container"); 
+        const parentRect = form.getBoundingClientRect();
+    
+        const buttonWidth = noButton.offsetWidth;
+        const buttonHeight = noButton.offsetHeight;
+    
+        const maxX = parentRect.width - buttonWidth;
+        const maxY = parentRect.height - buttonHeight;
+    
+        const randomX = Math.random() * maxX;
+        const randomY = Math.random() * maxY;
+    
+        noButton.style.position = "absolute";
+        noButton.style.left = `${randomX/1.5}px`;
+        noButton.style.top = `${randomY}px`;
+        noButton.textContent = "NO😜";
+        noButton.style.width = "60px";
+        noButton.style.height = "60px";
+    });
+
     siButton.addEventListener("click", () => {
         window.open("https://www.youtube.com/watch?v=-XHWQLHLPm4&ab_channel=TOMS");
     });
-    
-    
-    
-    
-    
+
 });
